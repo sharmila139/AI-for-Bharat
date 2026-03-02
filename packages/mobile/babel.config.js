1aafd25f63cbba122464a8846d8039b40a020d6a
Sarath2206 <sarathkumar.bommi@gmail.com>
2026-03-02 18:16:22 +0530

    Fix: Add TypeScript Babel plugin to handle React Native 0.76.9 syntax

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
+    '@babel/plugin-transform-typescript',
     ['@babel/plugin-transform-class-properties', { loose: true }],
     ['@babel/plugin-transform-private-methods', { loose: true }],
     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
